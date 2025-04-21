2f525355a58b74abbc664ccc832a197ee6c3071f
MAHESWARI1334 <maheswaritangirala@gmail.com>
2025-04-21 14:57:27 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,64 @@
+
+import pyttsx3
+import speech_recognition as sr
+import webbrowser
+import otp
+
+def speak(text):
+  engine = pyttsx3.init()
+  voices = engine.getProperty('voices')
+  engine.setProperty('voice',voices[1].id)
+  engine.say(text)
+  engine.runAndWait() 
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak to recognize.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            print("Recognizing......")
+            query = r.recognize_google(audio,language="en-in")
+            print(query)
+        except:
+            print("Speak Again")
+            return False
+        else:
+            return query
+def greet():
+    greeting = "Hello, How can i help You ?"
+    speak(greeting)
+greet()
+while True:
+    query = takecommand()
+    if query == False:
+        pass
+    elif "how are you" in query.lower():
+        speak("I am doing Good, How about you !")
+    elif "shut down" in query.lower():
+        speak("Take Care, Bye !")
+        break
+    elif "open web page" in query.lower():
+        speak("Opening Codegnan Website !")
+        webbrowser.open("www.codegnan.com")
+    elif "send otp" in query.lower():
+        speak("Enter Email address: ")
+        x = input("Enter email here: ")
+        otp.otp(x)
+        speak("Otp Sent Sucessfully")
+    elif "open web page" in query.lower():
+        speak("Opening Amazon Website !")
+        webbrowser.open("www.amazon.com")
+    elif "open web page" in query.lower():
+        speak("Opening Ajio Website !")
+        webbrowser.open("www.ajio.com")
+    elif "open web page" in query.lower():
+        speak("Opening Flipkart Website !")
+        webbrowser.open("www.flipkart.com")
+
+        
+    
+
+        
+
+
